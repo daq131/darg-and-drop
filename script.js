19d52b0d80ef583d4165e28872e42702f36e8490
daq131 <daq131@gmail.com>
2022-06-22 08:05:42 +0200

    rebuild arrays and update local storage

--- script.js
+++ script.js
@@ -94,13 +94,35 @@ function updateDOM() {
     createItemEl(onHoldList, 0, onHoldItem, index);
   })
   // Run getSavedColumns only once, Update Local Storage
-
-
+  updatedOnLoad = true;
+  updateSavedColumns();
 }
+
+// allows arrays to reflect drag and drop items
+function rebuildArrays() {
+  
+  backlogListArray = [];
+  for( let i=0; i < backlogList.children.length; i++) {
+    backlogListArray.push(backlogList.children[i].textContent);
+  }
+  progressListArray = [];
+  for( let i=0; i < progressList.children.length; i++) {
+    progressListArray.push(progressList.children[i].textContent);
+  }
+  completeListArray = [];
+  for( let i=0; i < completeList.children.length; i++) {
+    completeListArray.push(completeList.children[i].textContent);
+  }
+  onHoldListArray = [];
+  for( let i=0; i < onHoldList.children.length; i++) {
+    onHoldListArray.push(onHoldList.children[i].textContent);
+  }
+  updateDOM();
+}
+
 //when item start dragging
 function drag(e) {
   draggedItem = e.target;
-  console.log('draggedItem', draggedItem);
 }
 
 // column allows for Item to Drop
@@ -125,6 +147,7 @@ listColumns.forEach((column) => {
 // add item to column
 const parent = listColumns[currentColumn];
 parent.appendChild(draggedItem);
+rebuildArrays();
 }
 
 
